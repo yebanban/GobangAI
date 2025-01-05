be74cbfe4b41050dbe259fb37dccab1302af2f5a
fengfeng <372625968@qq.com>
2025-01-05 19:54:46 +0800

    打包

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import Unocss from 'unocss/vite'
 import { presetUno, presetAttributify, presetIcons } from 'unocss'
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [
     vue(),
     Unocss({
